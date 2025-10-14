ac7e4f45a8b6c347a1354288fd4920ea255dbd55
Chirani Rajapaksha <chirani.rajapaksha.97@gmail.com>
2025-10-15 00:04:06 +0530

    contact entry

--- portfolio-project/src/components/contact/Contact.jsx
+++ portfolio-project/src/components/contact/Contact.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import mail_icon from '../../assets/mail_icon.svg'
+import location_icon from '../../assets/location_icon.svg'
+import call_icon from '../../assets/call_icon.svg'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+        <div className='contact-title'>
+            <h1>Get in touch</h1>
+        </div>
+        <div className='contact-section'>
+            <div className='contact-left'>
+                <h1>Let's talk</h1>
+                <p>I am currently available for new job opportunities.</p>
+                <div className='contact-details'>
+                    <div className='contact-detail'>
+                        <img src={mail_icon} alt=''/> <p>chirani.rajapaksha.97@gmail.com</p>
+                    </div>
+                    <div className='contact-detail'>
+                        <img src={call_icon} alt=''/> <p>+94-770716994</p>
+                    </div>
+                    <div className='contact-detail'>
+                        <img src={location_icon} alt=''/> <p>Galle, Sri Lanka</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
