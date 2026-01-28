072af3996b5b4c8c794c0d51f3102a31f2603ad4
Chirani Rajapaksha <chirani.rajapaksha.97@gmail.com>
2026-01-28 16:31:15 +0530

    fix: base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/My-Portforlio/',
+  base: '/My-Portforlio',
   plugins: [
     react(),
     tailwindcss(),
